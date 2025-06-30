2a01092d909f660f49eca58ca952161a9670f459
Jesus Carranza <eduardo.carranza.mercado@gmail.com>
2025-06-30 17:39:32 -0600

    pagina donde se muestran los tableros

---
+++ src/app/app/boards/clientpage.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { CreateBoard } from "@/actions/board/createBoard";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { ArrowRight, Plus } from "lucide-react";
+import { Session } from "next-auth";
+import Link from "next/link";
+
+export default function BoardsPage({
+  user,
+  boardlist,
+}: {
+  user: Session["user"];
+  boardlist: { name: string; id: number; companyId: number }[];
+}) {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <p className="w-fit rounded-none border-0 bg-transparent scroll-m-20 text-2xl font-semibold tracking-tight">
+        Tableros ({boardlist.length})
+      </p>
+      {boardlist.length < 1 ? (
+        <div className="flex flex-col items-center w-full gap-2">
+          <p className="text-muted-foreground">
+            No hay tableros, agrega uno nuevo
+          </p>
+          <Button
+            onClick={() => {
+              CreateBoard({
+                name: "sin nombre",
+                companyId: Number(user.companyId),
+              });
+            }}
+            variant={"ghost"}
+          >
+            <Plus className={"h-5 w-5 mr-2"} />
+            Nuevo Tablero
+          </Button>
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="grid gap-4 md:grid-cols-2 md:gap-4 lg:grid-cols-4 xl:grid-cols-5">
+        {boardlist.map((board) => (
+          <Card key={"area-" + board.name} className="p-2">
+            <CardHeader className="pb-0"></CardHeader>
+            <CardContent>
+              <Button className="text-md" variant={"ghost"} asChild>
+                <Link href={"/app/board?board=" + board.id}>
+                  {board.name}
+                  <ArrowRight className="ml-2 h-4 w-4" />
+                </Link>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ src/app/app/boards/page.tsx
@@ -0,0 +1,59 @@
+import { redirect } from "next/navigation";
+import BoardsPage from "./clientpage";
+import { auth } from "@/lib/auth";
+import { db as database } from "@/db";
+import { boards, userBoardResponsabiliy, users } from "@/db/schema";
+import { sql } from "drizzle-orm";
+
+export default async function BoardsPageS() {
+  const session = await auth();
+
+  if (session) {
+    if (!session.user) {
+      redirect("/login");
+    }
+  } else {
+    redirect("/login");
+  }
+  const user = session.user;
+  const db = await database;
+
+  const [userInfo] = await db
+    .select({
+      id: users.id,
+      username: users.id,
+      role: users.role,
+      status: users.status,
+    })
+    .from(users)
+    .where(sql`${users.id}=${user.id}`);
+
+  if (userInfo.role == "worker") {
+    redirect("/app/tasks?user=" + userInfo.id);
+  }
+
+  const userModeratorBoardResps = await db
+    .select({ boardId: userBoardResponsabiliy.boardId })
+    .from(userBoardResponsabiliy)
+    .where(sql`${userBoardResponsabiliy.userId}=${userInfo.id}`);
+
+  const sqlWhereQuery = sql`${boards.companyId}=${user.companyId}`;
+
+  if (userInfo.role == "board_moderator") {
+    userModeratorBoardResps.map(({ boardId }, index) => {
+      if (index == 0) {
+        sqlWhereQuery.append(sql` and ${boards.id}=${boardId}`);
+      } else {
+        sqlWhereQuery.append(sql` or ${boards.id}=${boardId}`);
+      }
+    });
+  }
+
+  const boardList = await db.select().from(boards).where(sqlWhereQuery);
+
+  return (
+    <>
+      <BoardsPage user={user} boardlist={boardList}></BoardsPage>
+    </>
+  );
+}
